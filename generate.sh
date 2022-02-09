c88d8f1cc61e7b6b81d884044031b9b0ac5e1b10
Sean Davis <43555662+laughingbiscuit@users.noreply.github.com>
2022-02-09 13:49:58 +0000

    Create generate.sh

---
+++ generate.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+pandoc -s page.md -o index.html
+sed -i 's/<head>/<head><link rel="stylesheet" href="https:\/\/unpkg.com\/sakura.css\/css\/sakura-ink.css" type="text\/css">/' index.html
+sed -i 's/<img/<img style="display:block;margin:auto;" /g' index.html
