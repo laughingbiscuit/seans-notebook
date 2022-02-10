42833d3dfa7be46ca953bd66aa80d6199fc0a58f
Sean Davis <43555662+laughingbiscuit@users.noreply.github.com>
2022-02-10 07:59:44 +0000

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -3,3 +3,4 @@ set -e
 pandoc -s page.md -o index.html
 sed -i 's/<head>/<head><link rel="stylesheet" href="https:\/\/unpkg.com\/sakura.css\/css\/sakura-ink.css" type="text\/css">/' index.html
 sed -i 's/<img/<img style="display:block;margin:auto;" /g' index.html
+sed -i 's/<h1/<h1 style="text-align:center;"/' index.html
