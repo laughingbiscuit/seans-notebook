a2b322864f8e0d14c7b619369e6746a6e1f12af9
laughingbiscuit <sean@laughingbiscuit.com>
2022-02-23 16:44:00 +0000

    switch to water css

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 pandoc -s page.md -o index.html
-sed -i 's/<head>/<head><link rel="stylesheet" href="https:\/\/unpkg.com\/sakura.css\/css\/sakura-ink.css" type="text\/css">/' index.html
+sed -i 's/<head>/<head><link rel="stylesheet" href="https:\/\/cdn.jsdelivr.net\/npm\/water.css@2\/out\/light.css">/' index.html
 sed -i 's/<img/<img style="display:block;margin:auto;" /g' index.html
 sed -i 's/<h1/<h1 style="text-align:center;"/' index.html
