0e77ead7aabd8da8fa3663087a2ada2deaa7dd1c
laughingbiscuit <sean@laughingbiscuit.com>
2022-02-24 14:05:03 +0000

    set dir name to docs

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 set -e
 
 for FILE in `ls *.md`; do
-  OUT="./dist/$(echo $FILE | sed 's/\.md$/\.html/')"
+  OUT="./docs/$(echo $FILE | sed 's/\.md$/\.html/')"
   pandoc -s $FILE -o $OUT
   sed -i 's/<head>/<head><link rel="stylesheet" href="https:\/\/cdn.jsdelivr.net\/npm\/water.css@2\/out\/light.css">/' $OUT
   sed -i 's/<img/<img style="display:block;margin:auto;" /g' $OUT
