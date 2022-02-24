4eaf25a38facaeb7ee60e21200f3c7e090f02d76
laughingbiscuit <sean@laughingbiscuit.com>
2022-02-24 13:49:14 +0000

    create dist file for site

--- generate.sh
+++ generate.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
-pandoc -s page.md -o index.html
-sed -i 's/<head>/<head><link rel="stylesheet" href="https:\/\/cdn.jsdelivr.net\/npm\/water.css@2\/out\/light.css">/' index.html
-sed -i 's/<img/<img style="display:block;margin:auto;" /g' index.html
-sed -i 's/<h1/<h1 style="text-align:center;"/' index.html
+
+for FILE in ls *.md; do
+  OUT="./dist/$(echo $FILE | sed 's/\.md$/\.html/')"
+  pandoc -s $FILE -o $OUT
+  sed -i 's/<head>/<head><link rel="stylesheet" href="https:\/\/cdn.jsdelivr.net\/npm\/water.css@2\/out\/light.css">/' $OUT
+  sed -i 's/<img/<img style="display:block;margin:auto;" /g' $OUT
+  sed -i 's/<h1/<h1 style="text-align:center;"/' $OUT
+done
